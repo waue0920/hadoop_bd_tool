2806074140f125e619920643deb0b7beba40c416
waue0920 <waue0920@gmail.com>
2016-03-28 16:00:28 +0800

    fix bug on 2016/3/28

--- cloudera_mgr_admin/toolkit/purge_cloudera_manager.sh
+++ cloudera_mgr_admin/toolkit/purge_cloudera_manager.sh
@@ -2,6 +2,14 @@
 # http://www.cloudera.com/content/cloudera/en/documentation/core/latest/topics/cm_ig_uninstall_cm.html
 # https://niksammy.wordpress.com/2014/04/21/uninstalling-cdh5-cluster/
 
+ret=$(which apt-get);
+ret=$?
+if [ $ret -ne 0 ];then
+        echo "[ERROR] this script is only support for debian system, please transform it by your self. "
+	exit 1 ;
+fi
+
+
 service cloudera-scm-server stop
 service cloudera-scm-server-db stop
 service cloudera-scm-agent stop
